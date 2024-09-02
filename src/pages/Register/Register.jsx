212346a2f9b5dc67b25b79e8bb1931511d5a0f88
yhmunna1 <yh.munna.barisal@gmail.com>
2024-09-03 01:46:54 +0600

    register page design

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,96 @@
+import Navbar from "../Shared/Navbar/Navbar";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("password"));
+  };
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="bg-gray-100 my-10 py-20 rounded-md">
+        <h2 className="text-4xl font-semibold text-gray-600 text-center">
+          Register your account
+        </h2>
+        <hr className="my-5" />
+        {/* Form */}
+        <form
+          onSubmit={handleRegister}
+          className="card-body md:w-3/4 lg:w-2/4 mx-auto"
+        >
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold text-xl">
+                Your Name
+              </span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold text-xl">
+                Photo URL
+              </span>
+            </label>
+            <input
+              type="text"
+              name="photo"
+              placeholder="Enter your photo url"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold text-xl">
+                Email address
+              </span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-semibold text-xl">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral">Register</button>
+          </div>
+          <label className="label">
+            <Link
+              to="/login"
+              className="label-text-alt link link-hover font-medium text-base"
+            >
+              Already Have An Account ?{" "}
+              <span className="text-red-500">Login</span>
+            </Link>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
